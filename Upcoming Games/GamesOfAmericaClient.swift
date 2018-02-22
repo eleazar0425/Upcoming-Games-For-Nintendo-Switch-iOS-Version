470b11910c2882f37899d6eef8136a08a4f189e6
Eleazar Estrella <eestrella0425@gmail.com>
2018-02-21 22:15:39 -0400

    structuring folders

--- Upcoming Games/GamesOfAmericaClient.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GamesOfAmericaClient.swift
-//  Upcoming Games
-//
-//  Created by Eleazar Estrella on 7/17/17.
-//  Copyright © 2017 Eleazar Estrella. All rights reserved.
-//
-
-import Foundation
